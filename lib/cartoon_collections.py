2e4bdede6962e29b57afd3abf2961771a9654b59
Krystle Lee <l33.krys@gmail.com>
2023-09-17 22:21:45 -0700

    complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,22 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(list):
+    for index, dwarf in enumerate(list):
+        print(f"{index+1}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(list):
+    return [f"{planeteer.title()}!" for planeteer in list]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(list):
+    if len([item for item in list if len(item) > 4]):
+        return True
+    else:
+        return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(list):
+    if "cheddar" in list:
+        return "cheddar"
+    elif "gouda" in list:
+        return "gouda"
+    elif "camembert" in list:
+        return "camembert"
+    else:
+        return None
